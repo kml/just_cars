8e6ce50496a3a1d46c0be4f421bade90afbe41a2
kml <kamil.lemanski@gmail.com>
2019-10-12 13:20:48 +0200

    tests fix

--- test/models/offer_test.rb
+++ test/models/offer_test.rb
@@ -3,31 +3,31 @@ require 'test_helper'
 class OfferTest < ActiveSupport::TestCase
   test "title blank is invalid" do
     offer = Offer.new(title: nil)
-    offer.valid?
+    assert offer.invalid?
     assert offer.errors[:title].present?
   end
 
   test "description blank is invalid" do
     offer = Offer.new(description: nil)
-    offer.valid?
+    assert offer.invalid?
     assert offer.errors[:description].present?
   end
 
   test "price blank is invalid" do
     offer = Offer.new(price: nil)
-    offer.valid?
+    assert offer.invalid?
     assert offer.errors[:price].present?
   end
 
   test "price negative is invalid" do
     offer = Offer.new(price: -1)
-    offer.valid?
+    assert offer.invalid?
     assert offer.errors[:price].present?
   end
 
   test "price zero is invalid" do
     offer = Offer.new(price: -1)
-    offer.valid?
+    assert offer.invalid?
     assert offer.errors[:price].present?
   end
 end
