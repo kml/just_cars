954410d95c5dad4d1cd980bff67700bb7994e869
kml <kamil.lemanski@gmail.com>
2019-10-12 12:24:50 +0200

    seeding of offers for local development

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do
+  now = Time.now
+
+  offer = Offer.create(
+    title: Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraphs(number: 2).join(" "),
+    price: rand(2_000..30_000),
+  )
+
+  offer.photo.attach(io: Rails.root.join("db", "seeds", "photo_1000x1000.png").open , filename: 'photo.png')
+end
