c8943c3382859286a4701d4b1eaf94d180253dfc
kml <kamil.lemanski@gmail.com>
2019-10-12 12:39:04 +0200

    devise for basic authentication
    left registerable module

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,5 +1,6 @@
 class OffersController < ApplicationController
   before_action :set_offer, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   # GET /offers
   def index
