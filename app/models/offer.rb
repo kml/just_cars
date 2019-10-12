5bd56c4577f86b9c83c9be37ac23ce57bd4dc396
kml <kamil.lemanski@gmail.com>
2019-10-12 11:40:52 +0200

    base offer validations

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,3 +1,7 @@
 class Offer < ApplicationRecord
+  validates :title, presence: true
+  validates :price, numericality: {greater_than: 0}
+  validates :description, presence: true
+
   has_rich_text :description
 end
--- test/models/offer_test.rb
+++ test/models/offer_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class OfferTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "title blank is invalid" do
+    offer = Offer.new(title: nil)
+    offer.valid?
+    assert offer.errors[:title].present?
+  end
+
+  test "description blank is invalid" do
+    offer = Offer.new(description: nil)
+    offer.valid?
+    assert offer.errors[:description].present?
+  end
+
+  test "price blank is invalid" do
+    offer = Offer.new(price: nil)
+    offer.valid?
+    assert offer.errors[:price].present?
+  end
+
+  test "price negative is invalid" do
+    offer = Offer.new(price: -1)
+    offer.valid?
+    assert offer.errors[:price].present?
+  end
+
+  test "price zero is invalid" do
+    offer = Offer.new(price: -1)
+    offer.valid?
+    assert offer.errors[:price].present?
+  end
 end
